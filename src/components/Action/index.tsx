220cf20ca5de0e65d7420389b5ba119f95ae9fc3
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-05-28 18:03:44 +0200

    feat(Action): change style + add ActionTypes

--- src/components/Action/index.tsx
+++ src/components/Action/index.tsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from 'react';
 import gatherIcon from '../../../public/assembly_icon.png';
 import tentIcon from '../../../public/campground_icon.png';
 import pinIcon from '../../../public/pin_icon.png';
+import mapIcon from '../../../public/map_icon.png';
+import menuIcon from '../../../public/menu_icon.png';
+import closeIcon from '../../../public/close_icon.png';
+import infoIcon from '../../../public/info_icon.png';
 
 import styles from './Action.module.css';
 
@@ -10,6 +14,10 @@ export enum ActionTypes {
     gather = 'gather',
     tent = 'tent',
     pinpoint = 'pinpoint',
+    map = 'map',
+    menu = 'menu',
+    close = 'close',
+    info = 'info',
 }
 
 interface ActionProps {
@@ -17,7 +25,7 @@ interface ActionProps {
 }
 
 export const Action = ({ type }: ActionProps) => {
-    let [icon, setIcon] = useState(<span />);
+    const [icon, setIcon] = useState(<span />);
 
     useEffect(() => {
         switch (type) {
@@ -29,6 +37,18 @@ export const Action = ({ type }: ActionProps) => {
                 break;
             case ActionTypes.pinpoint:
                 setIcon(<img src={pinIcon.src} alt='map pinpoint icon' width='22.5px' height='auto' />);
+                break;
+            case ActionTypes.map:
+                setIcon(<img src={mapIcon.src} alt='map icon' width='28.5px' height='auto' />);
+                break;
+            case ActionTypes.menu:
+                setIcon(<img src={menuIcon.src} alt='menu icon' width='27px' height='auto' />);
+                break;
+            case ActionTypes.close:
+                setIcon(<img src={closeIcon.src} alt='close icon' width='25.5px' height='auto' />);
+                break;
+            case ActionTypes.info:
+                setIcon(<img src={infoIcon.src} alt='info icon' width='11px' height='auto' />);
                 break;
         }        
     }, [type]);
