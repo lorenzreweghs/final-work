b004ddc2b7aca1000cd691be193c63864ecf7b75
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-05-30 14:48:09 +0200

    feat(map): add functionality to add Action markers

--- pages/map/[session].tsx
+++ pages/map/[session].tsx
@@ -1,15 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import mapboxgl, { GeoJSONSource } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { ref, onValue, get } from "firebase/database";
+import Swal from 'sweetalert2';
 
 import useLocation, { db } from '../../src/hooks/useLocation';
-import useSession from '../../src/hooks/useSession';
+import useSession, { MarkerTypes } from '../../src/hooks/useSession';
 import useOtherUser from '../../src/hooks/useOtherUser';
 import { Navigation } from '../../src/components/Navigation';
 import { Action, ActionTypes } from '../../src/components/Action';
+
+import flagIcon from '../../public/flag_icon_color.png';
+import tentIcon from '../../public/campground_icon.png';
 
 import styles from '../../styles/Map.module.css';
 
@@ -18,12 +22,14 @@ const Map = () => {
 
     const router = useRouter();
 
-    const { updateLocation } = useLocation();
-    const { getUsersInSession, updateUserStatus } = useSession();
+    const { updateLocation, addMarker } = useLocation();
+    const { getUsersInSession, getMarkersInSession, updateUserStatus } = useSession();
     const { getUserName } = useOtherUser();
 
-    const [activeSession, setActiveSession] = useState<string | string[] | undefined>("XXXXXX");
+    const [activeSession, setActiveSession] = useState<string | string[] | undefined>('XXXXXX');
     const [navIsOpen, setNavIsOpen] = useState(false);
+
+    const [activeAction, setActiveAction] = useState('');
 
     useEffect(() => {
         if (!router.isReady || isLoading) return;
@@ -119,6 +125,197 @@ const Map = () => {
             });
         });
 
+        map.on('click', (event) => {
+            switch (activeAction) {
+                case "gather":
+                    handleGather(event);
+                    break;
+                case "tent":
+                    handleTent(event);
+                    break;
+                case "pinpoint":
+                    handlePinpoint(event);
+                    break;
+            }
+        });
+
+        const setCurrentFlag = async () => {
+            const [coords] = await getMarkersInSession(activeSession, MarkerTypes.flag);
+            if (coords) {
+                map.loadImage(flagIcon.src, async (error, image) => {
+                    if (error) throw error;
+    
+                    map.addImage('flag', image!);
+
+                    const geojson = await getGeoJson(coords.lat, coords.lng);
+                    map.addSource('flag', {
+                        type: 'geojson',
+                        data: geojson
+                    });
+        
+                    map.addLayer({
+                        'id': 'gather',
+                        'type': 'symbol',
+                        'source': 'flag',
+                        'layout': {
+                            'icon-image': 'flag',
+                            'icon-size': 0.25,
+                        }
+                    });
+                });
+            }
+        }
+        setCurrentFlag();
+
+        const handleGather = async (event: mapboxgl.MapMouseEvent) => {
+            const coords = event.lngLat;
+            const geojson = await getGeoJson(coords.lat, coords.lng);
+            (map.getSource('flag') as GeoJSONSource).setData(geojson);
+
+            setTimeout(() => {
+                Swal.fire({
+                    title: 'Vlag bevestigen',
+                    text: "Zeker dat je het verzamelpunt wilt verplaatsen?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: 'var(--color-primary-light)',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Vlag plaatsen',
+                    cancelButtonText: 'Annuleren'
+                }).then(async (result) => {
+                    if (result.isConfirmed) {
+                        await addMarker(activeSession, [{lng: coords.lng, lat: coords.lat}], true);
+                    } else if (result.dismiss === Swal.DismissReason.cancel) {
+                        const [coords] = await getMarkersInSession(activeSession, MarkerTypes.flag);
+                        const geojson = await getGeoJson(coords.lat, coords.lng);
+                        (map.getSource('flag') as GeoJSONSource).setData(geojson);
+                    }
+                    setActiveAction("");
+                });
+            }, 1000);
+        }
+
+        const setCurrentTent = async () => {
+            const [coords] = await getMarkersInSession(activeSession, MarkerTypes.tent);
+            if (coords) {
+                map.loadImage(tentIcon.src, async (error, image) => {
+                    if (error) throw error;
+ 
+                    map.addImage('campground', image!);
+
+                    const geojson = await getGeoJson(coords.lat, coords.lng);
+                    map.addSource('campground', {
+                        type: 'geojson',
+                        data: geojson
+                    });
+    
+                    map.addLayer({
+                        'id': 'tent',
+                        'type': 'symbol',
+                        'source': 'campground',
+                        'layout': {
+                            'icon-image': 'campground',
+                        }
+                    });
+                });
+            }
+        }
+        setCurrentTent();
+
+        const handleTent = async (event: mapboxgl.MapMouseEvent) => {
+            const coords = event.lngLat;
+            const geojson = await getGeoJson(coords.lat, coords.lng);
+            if (map.getSource('campground')) {
+                (map.getSource('campground') as GeoJSONSource).setData(geojson);
+            } else {
+                map.loadImage(tentIcon.src, async (error, image) => {
+                    if (error) throw error;
+ 
+                    map.addImage('campground', image!);
+
+                    map.addSource('campground', {
+                        type: 'geojson',
+                        data: geojson
+                    });
+    
+                    map.addLayer({
+                        'id': 'tent',
+                        'type': 'symbol',
+                        'source': 'campground',
+                        'layout': {
+                            'icon-image': 'campground',
+                        }
+                    });
+                });
+            }
+
+            setTimeout(() => {
+                Swal.fire({
+                    title: 'Locatie op camping bevestigen',
+                    text: "Zeker dat je hier de locatie van jullie tenten wilt plaatsen?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: 'var(--color-primary-light)',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Pinpoint plaatsen',
+                    cancelButtonText: 'Annuleren'
+                }).then(async (result) => {
+                    if (result.isConfirmed) {
+                        await addMarker(activeSession, [{lng: coords.lng, lat: coords.lat}], false, true);
+                    } else if (result.dismiss === Swal.DismissReason.cancel) {
+                        const [coords] = await getMarkersInSession(activeSession, MarkerTypes.tent);
+                        if (coords) {
+                            const geojson = await getGeoJson(coords.lat, coords.lng);
+                            (map.getSource('campground') as GeoJSONSource).setData(geojson);                            
+                        } else {
+                            map.removeLayer('tent');
+                            map.removeSource('campground');
+                        }
+                    }
+                    setActiveAction("");
+                });
+            }, 1000);
+        }
+
+        const setCurrentMarkers = async () => {
+            const coordsArray = await getMarkersInSession(activeSession);
+            if (coordsArray.length) {
+                coordsArray.forEach((coords) => {
+                    const marker = new mapboxgl.Marker();
+                    marker.setLngLat(coords).addTo(map);
+                })
+            }
+        }
+        setCurrentMarkers();
+
+        const handlePinpoint = async (event: mapboxgl.MapMouseEvent) => {
+            const markerArray = await getMarkersInSession(activeSession);
+
+            const marker = new mapboxgl.Marker();
+            const coords = event.lngLat;
+            marker.setLngLat(coords).addTo(map);
+
+            setTimeout(() => {
+                Swal.fire({
+                    title: 'Pinpoint bevestigen',
+                    text: "Zeker dat je een pinpoint wilt plaatsen?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: 'var(--color-primary-light)',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Pinpoint plaatsen',
+                    cancelButtonText: 'Annuleren'
+                }).then(async (result) => {
+                    if (result.isConfirmed) {
+                        await addMarker(activeSession, [...markerArray, {lng: coords.lng, lat: coords.lat}]);
+                    } else if (result.dismiss === Swal.DismissReason.cancel) {
+                        marker.remove();
+                    }
+                    setActiveAction("");
+                });
+            }, 1000);
+        }
+
         navigator.geolocation.watchPosition(handlePositionUpdate, handlePositionError, { enableHighAccuracy: true });
         
         document.addEventListener("visibilitychange", async () => {
@@ -133,7 +330,7 @@ const Map = () => {
             await updateUserStatus(user?.sub!, false);
         }, false);
 
-    }, [router.isReady, isLoading]);
+    }, [router.isReady, isLoading, getMarkersInSession]);
 
     const getGeoJson = (lat: number, lng: number): any => {
         return {
@@ -166,15 +363,24 @@ const Map = () => {
                 <Action type={ActionTypes.menu} />
             </div>
             <div className={styles.actions}>
-                <div>
-                    <Action type={ActionTypes.gather} />
+                <div onClick={() => {
+                    if (activeAction === 'gather') setActiveAction('');
+                    else setActiveAction('gather');
+                }}>
+                    <Action type={ActionTypes.gather} isActive={activeAction === 'gather'} />
                 </div>
-                <div>
-                    <Action type={ActionTypes.tent} />
+                <div onClick={() => {
+                    if (activeAction === 'tent') setActiveAction('');
+                    else setActiveAction('tent');
+                }}>
+                    <Action type={ActionTypes.tent} isActive={activeAction === 'tent'} />
                 </div>
-                <div>
-                    <Action type={ActionTypes.pinpoint} />
-                </div>          
+                <div onClick={() => {
+                    if (activeAction === 'pinpoint') setActiveAction('');
+                    else setActiveAction('pinpoint');
+                }}>
+                    <Action type={ActionTypes.pinpoint} isActive={activeAction === 'pinpoint'} />
+                </div>
             </div>
         </div>
     );
