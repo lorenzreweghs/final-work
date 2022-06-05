8a6a799b355d22ccde5af56b5e0921c51ceb5ae3
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-06-05 21:56:06 +0200

    refactor(badges): change order to design

--- config/badges.ts
+++ config/badges.ts
@@ -1,12 +1,12 @@
 import { StaticImageData } from 'next/image';
 
-import firstBadge from '../public/badges/reward_icon.png';
-import secondBadge from '../public/badges/reward_icon_2.png';
-import thirdBadge from '../public/badges/reward_icon_3.png';
-import fourthBadge from '../public/badges/reward_icon_4.png';
-import fifthBadge from '../public/badges/reward_icon_5.png';
-import sixthBadge from '../public/badges/reward_icon_6.png';
-import seventhBadge from '../public/badges/reward_icon_7.png';
+import firstBadge from '../public/badges/reward_icon_7.png';
+import secondBadge from '../public/badges/reward_icon.png';
+import thirdBadge from '../public/badges/reward_icon_2.png';
+import fourthBadge from '../public/badges/reward_icon_6.png';
+import fifthBadge from '../public/badges/reward_icon_3.png';
+import sixthBadge from '../public/badges/reward_icon_4.png';
+import seventhBadge from '../public/badges/reward_icon_5.png';
 import winnerBadge from '../public/badges/winner_icon.png';
 
 export const badges: Array<StaticImageData> = [
