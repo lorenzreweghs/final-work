e9d4a57a1bf60ebe57c42b9fce4af5f19bd0dd34
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-06-08 18:55:50 +0200

    feat: create Destop component & add background-image

---
+++ pages/desktop.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Desktop.module.css';
+
+const Desktop = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default Desktop;
