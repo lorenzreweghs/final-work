7269e90cca36df8ab5d3e97771a5cd98076e9e31
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-06-08 19:25:10 +0200

    feat(desktop): add title & subTitle

--- pages/desktop.tsx
+++ pages/desktop.tsx
@@ -3,7 +3,8 @@ import styles from '../styles/Desktop.module.css';
 const Desktop = () => {
     return (
         <div className={styles.container}>
-
+            <h1 className={styles.title}>Festicon</h1>
+            <h2 className={styles.subTitle}>Gebruik een <span>smartphone</span><br />om op het platform te geraken</h2>
         </div>
     );
 }
