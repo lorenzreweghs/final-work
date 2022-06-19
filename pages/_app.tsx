747b9d57fc783fc9d7c996cae597032fd39991f8
Lorenz Reweghs <lorenz.r99@gmail.com>
2022-06-19 20:41:00 +0200

    fix(app): import router to check router.pathname

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,29 @@ import { UserProvider } from '@auth0/nextjs-auth0';
 
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(() => {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
-  });
+    if (!router.isReady) return;
+
+    const setWindowHeight = () => {
+      document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+    }
+    setWindowHeight();
+
+    if (router.pathname !== '/session') {
+      window.addEventListener('resize', setWindowHeight);
+    }
+
+    return () => {
+      window.removeEventListener('resize', setWindowHeight);
+    }    
+  }, [router.isReady, router.pathname]);
 
   return (
     <UserProvider>
